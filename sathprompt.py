b225be91ad8097b9fbd4d9ba4a063c894a59cabe
bradsprigg <brad_sprigg@data3.com.au>
2022-11-17 16:04:53 +1030

    added new combine methods

--- sathprompt.py
+++ sathprompt.py
@@ -3,9 +3,10 @@ import random
 from enum import Enum
 
 class promptFragment:
-    def __init__(self, prompt: str, numSelections: int, weight: decimal):
+    def __init__(self, prompt:str, numSelections:int, weight:decimal, variance:decimal=0):
         self.prompt = prompt
         self.weight = weight
+        self.variance = variance
         self.numSelections = numSelections
 
     def asIs(self):
@@ -21,16 +22,22 @@ class promptFragment:
         selectPrompt = random.choices(splitPrompt, k=self.numSelections)
         return " ".join([s + f":{self.weight}" for s in selectPrompt])
 
+    def selectNumWithRandWeight(self):
+        splitPrompt = self.prompt.split(", ")
+        selectPrompt = random.choices(splitPrompt, k=self.numSelections)
+        return " ".join([s + f":{round(random.uniform(self.weight - self.variance, self.weight + self.variance),2)}" for s in selectPrompt])
+
 class CombineMethod(Enum):
     AS_IS = 1
     SELECT_NUM = 2
     SELECT_NUM_WITH_WEIGHT = 3
+    SELECT_NUM_WITH_RAND_WEIGHT = 4
 
 class promptFragments:
     fragments = []
 
-    def addFragment(self, prompt: str, numSelections: int, weight: decimal):
-        self.fragments.append(promptFragment(prompt, numSelections, weight))
+    def addFragment(self, prompt: str, numSelections: int, weight: decimal, variance: decimal=0):
+        self.fragments.append(promptFragment(prompt, numSelections, weight, variance))
 
     def combineFragments(self, method: CombineMethod):
         if method == CombineMethod.AS_IS:
@@ -39,6 +46,9 @@ class promptFragments:
             return ", ".join([f.selectNum() for f in self.fragments])
         elif method == CombineMethod.SELECT_NUM_WITH_WEIGHT:
             return " ".join([f.selectNumWithWeight() for f in self.fragments])
+        elif method == CombineMethod.SELECT_NUM_WITH_RAND_WEIGHT:
+            return " ".join([f.selectNumWithRandWeight() for f in self.fragments])
+
         
 
 
