2e9689352ef9e1a20d35e2a3a82b91ce41e498df
bradsprigg <brad_sprigg@data3.com.au>
2022-12-11 13:47:59 +1030

    renamed sathprompt

--- testsathprompt.py
+++ testsathprompt.py
@@ -1,14 +1,14 @@
-import sathprompt
+import promptexplorer
 from synonymiser import synonymiser
 
-x = sathprompt.promptFragments()
+x = promptexplorer.promptFragments()
 x.addFragment("A cow, A bird, A camel", 1, 1)
 x.addFragment("by John, by Bill, by Jane, by Sarah, by Mary", 2, 0.5)
 x.addFragment("in the field, in the forest, in the desert", 1, 0.5)
 x.addFragment("with a hat, with a scarf, with a jacket", 1, -0.5)
 #x.addFragment("normal, poor quality, boring, tedious, interesting, exciting", 3, -0.5)
-pos = x.combineFragments(sathprompt.CombineMethod.SELECT_NUM_DIRECTIONAL, sathprompt.Direction.POSITIVE)
-neg = x.combineFragments(sathprompt.CombineMethod.SELECT_NUM_DIRECTIONAL, sathprompt.Direction.NEGATIVE)
+pos = x.combineFragments(promptexplorer.CombineMethod.SELECT_NUM_DIRECTIONAL, promptexplorer.Direction.POSITIVE)
+neg = x.combineFragments(promptexplorer.CombineMethod.SELECT_NUM_DIRECTIONAL, promptexplorer.Direction.NEGATIVE)
 
 print(pos)
 print(neg)
