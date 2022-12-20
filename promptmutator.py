59c6b648a2fb58eed77e325e2c754878f159d87f
bradsprigg <brad_sprigg@data3.com.au>
2022-12-20 11:50:52 +1030

    new prompt mutator options

--- promptmutator.py
+++ promptmutator.py
@@ -2,6 +2,7 @@ import torch
 import gc
 import warnings
 import clip
+import re
 from IPython.display import HTML, display
 from random import sample
 warnings.filterwarnings('ignore')
@@ -19,7 +20,7 @@ perceptor.eval().float().requires_grad_(False);
 
 tokenizer = clip.simple_tokenizer.SimpleTokenizer()
 
-class synonymiser:
+class promptmutator:
     def __init__(self, prompt:str, topk:int):
         self.prompt = prompt
         self.topk = topk
@@ -28,10 +29,10 @@ class synonymiser:
     def asIs(self):
         return self.prompt
 
-    def synonymise(self):
-        target_tokens = tokenizer.encode(self.prompt)
-        new_prompt = ""
-        num_prompt = 0
+    def mutate_word(self, word):
+        target_tokens = tokenizer.encode(word)
+        new_word = ""
+        num_word = 0
         total_index = 0
         for now_token in target_tokens:
             target_emb = perceptor.token_embedding.weight[now_token,None].detach()
@@ -45,14 +46,33 @@ class synonymiser:
             shuffle_output = sample(output, len(output))
             new_token = shuffle_output[0][0]
             new_index = shuffle_output[0][1]
-            new_prompt += new_token
+            new_word += new_token
             total_index += new_index
-            num_prompt += 1
-        total_index = total_index / num_prompt
-        # strip any unicode characters from new_prompt
-        new_prompt = new_prompt.encode('ascii', 'ignore').decode('ascii')
-        # strip any commas from new_prompt
+            num_word += 1
+        total_index = total_index / num_word
+        # strip any unicode characters from new_word
+        new_word = new_word.encode('ascii', 'ignore').decode('ascii')
+        # strip any commas from new_word
         if self.stripcommas:
-            new_prompt = new_prompt.replace(", ","")
-        return new_prompt, total_index
+            new_word = new_word.replace(", ","")
+        return new_word, total_index
+
+    def mutate(self):
+        return self.mutate_word(self.prompt)
+
+    def mutate_specific_words(self):
+        # find all instances of words in the prompt enclosed in double square-brackets
+        # e.g. picture of [[cow]] riding a [[bike]]
+        # will find a list of the strings '[[cow]]' and '[[bike]]'
+        list_squared_brackets = re.findall(r'\[\[.*?\]\]', self.prompt)
+        # mutate each word in the list of words
+        for word in list_squared_brackets:
+            # remove the double square-brackets
+            word = word[2:-2]
+            # mutate the word
+            new_word, distance = self.mutate_word(word)
+            # replace the word in the prompt with the mutated word
+            self.prompt = self.prompt.replace(f'[[{word}]]', f'[[{new_word}]]')
+
+        
 
